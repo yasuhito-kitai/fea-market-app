e7826e9e797d133e88ff7c7596384c30c77dcf4d
y.kitai <y.kitai.1124@gmail.com>
2025-09-24 22:35:00 +0900

    localstrageへの保存、購入ページへの遷移処理完了

--- frontend/src/app/purchase/address/[item_id]/page.tsx
+++ frontend/src/app/purchase/address/[item_id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useParams } from "next/navigation";
+import { useParams,useRouter } from "next/navigation";
 
 export default function PurchaseAddressPage() {
     const [zipcode, setZipcode] = useState<string>("");
@@ -9,8 +9,21 @@ export default function PurchaseAddressPage() {
     const [building, setBuilding] = useState<string>("");
     const params = useParams();
     const itemId = Array.isArray(params.item_id)
-  ? params.item_id[0]   // 配列なら最初の要素を取る
-  : params.item_id ?? ""; // 文字列ならそのまま / 未定義なら空文字
+        ? params.item_id[0]   // 配列なら最初の要素を取る
+        : params.item_id ?? ""; // 文字列ならそのまま / 未定義なら空文字
+    const router = useRouter()
+    
+    function handleSave() {
+        const data = {
+        zipcode: zipcode,
+        address: address,
+        building: building,
+        };
+
+        localStorage.setItem("shippingDraft:" + itemId, JSON.stringify(data));
+        alert("保存しました");
+        router.push(`/purchase/${itemId}`);
+    }
     return (
         <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow">
             <h1 className="text-2xl font-bold mb-6">住所の変更</h1>
@@ -36,6 +49,7 @@ export default function PurchaseAddressPage() {
                     type="text"
                 />
             </div>
+            <button className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 disabled:opacity-60" onClick={handleSave}>更新する</button>
         </div>
     );
 }
